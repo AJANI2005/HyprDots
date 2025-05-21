7718f957e2999c5238159d5f32c2e8c23762dc0b
AJANI2005 <ajanismall2005@gmail.com>
2025-05-21 14:58:43 -0400

    restore nvim

--- nvim/eww/launchers/bar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# args is monitor numbers
-MONITORS=("$@")
-
-for MONITOR in "${MONITORS[@]}"; do
-  eww open --screen "$MONITOR" --id "bar$MONITOR" --toggle bar
-done
--- nvim/hypr/hyprbattery.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true; do
-  $HOME/Hypr/scripts/battery_monitor.sh
-  sleep 10  # Sleep for 60 seconds (1 minute) or adjust as needed
-done
-
